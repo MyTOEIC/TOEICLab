5e671dca017e6911c33f9b68bd8e9719fcba8874
MyTOEIC <ptra28082002@gmail.com>
2026-02-04 23:21:53 +0700

    Update data.js

--- data/tu-vung/bai-1/data.js
+++ data/tu-vung/bai-1/data.js
@@ -2,10 +2,19 @@
   "title": "Chủ đề: Hợp đồng",
   "content": "In a modern <b>office</b>, employees often use <b>copiers</b> and <b>fax machines</b>. The <b>manager</b> is responsible for supervising the team.",
   "items": [
-    { "en": "Abide by", "vi": "tuân theo", "pron": "/'ɔfis/" },
-    { "en": "Manager", "vi": "Quản lý", "pron": "/'mænidʒə/" },
-    { "en": "Colleague", "vi": "Đồng nghiệp", "pron": "/'kɔli:g/" },
-    { "en": "Equipment", "vi": "Thiết bị", "pron": "/i'kwipmənt/" }
+    { "en": "Abide by", "type": "(ph.v)", "vi": "tuân theo", "pron": "əˈbaɪd baɪ" },
+    { "en": "Agreement, "type": "(n)", "vi": "sự đồng ý, hợp đồng", "pron": "əˈɡriːmənt" },
+    { "en": "Assurance, "type": "(n)", "vi": "lời đảm bảo", "pron": "əˈʃʊrəns" },
+    { "en": "Cancellation, "type": "(n)", "vi": "sự bãi bỏ", "pron": "ˌkænsəˈleɪʃən" },
+    { "en": "Determine", "type": "(v)", "vi": "quyết định, xác định", "pron": "dɪˈtɜːrmɪn" }, 
+    { "en": "Engage", "type": "(v)", "vi": "tham gia, cam kết", "pron": "ɪnˈɡeɪdʒ" },
+    { "en": "Establish", "type": "(v)", "vi": "thành lập, củng cố", "pro": "ɪˈstæblɪʃ" }, 
+    { "en": "Obligate", "vi": "(v) bắt buộc, ép buộc (adj) có tính chất bắt buộc", "pron": "ɑːblɪɡeɪt"},
+    { "en": "Party", "type": "(n)", "vi": "phe, đảng phái", "pron": "pɑːrti"}, 
+    { "en": "Provision", "type": "(n)", "vi": "việc cung cấp, điều khoản", "pron": "prəˈvɪʒən"},
+    { "en": "Resolve", "type": "(v)", "vi": "giải quyết (n) sự quyết định", "pron": "rɪˈzɑːlv"},  
+    { "en": "Specific", "type": "(adj)", "vi": "riêng biệt", "pron": "spəˈsɪfɪk" }
   ]
 
 };
+
