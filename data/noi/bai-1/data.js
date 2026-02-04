04d13f165f00ad38ac6f4abffe6347488efed835
MyTOEIC <ptra28082002@gmail.com>
2026-02-04 22:40:35 +0700

    Add files via upload

---
+++ data/noi/bai-1/data.js
@@ -0,0 +1,12 @@
+﻿window.lessonData = {
+  "title": "Speaking Part 2: Describe a picture",
+  
+  // Tên file ảnh bạn đã chép vào folder
+  "image": "hinh-anh.jpg", 
+  
+  // Tên file audio mẫu (Sample answer)
+  "audio": "mau-tra-loi.mp3",
+  
+  // (Tùy chọn) Transcript bài nói mẫu để bạn tham khảo
+  "transcript": "In this picture, I can see a group of people in a meeting room. They are sitting around a large wooden table. The man in the blue shirt is pointing at a document..."
+};
