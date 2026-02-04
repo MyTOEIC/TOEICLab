ee2650188bef0e1a3ca22b9f5210a27cea1f9c56
MyTOEIC <ptra28082002@gmail.com>
2026-02-04 22:56:34 +0700

    Update data.js

--- data/tu-vung/bai-1/data.js
+++ data/tu-vung/bai-1/data.js
@@ -1,10 +1,11 @@
 ﻿window.lessonData = {
-  "title": "Chủ đề: Office (Văn phòng)",
+  "title": "Chủ đề: Hợp đồng",
   "content": "In a modern <b>office</b>, employees often use <b>copiers</b> and <b>fax machines</b>. The <b>manager</b> is responsible for supervising the team.",
   "items": [
-    { "en": "Office", "vi": "Văn phòng", "pron": "/'ɔfis/" },
+    { "en": "Abide by", "vi": "tuân theo", "pron": "/'ɔfis/" },
     { "en": "Manager", "vi": "Quản lý", "pron": "/'mænidʒə/" },
     { "en": "Colleague", "vi": "Đồng nghiệp", "pron": "/'kɔli:g/" },
     { "en": "Equipment", "vi": "Thiết bị", "pron": "/i'kwipmənt/" }
   ]
+
 };
