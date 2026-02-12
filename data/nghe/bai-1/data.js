3e028ebaffed06e18b75f0f43467c6c7817b7a02
MyTOEIC <ptra28082002@gmail.com>
2026-02-12 20:43:50 +0700

    Update data.js

--- data/nghe/bai-1/data.js
+++ data/nghe/bai-1/data.js
@@ -1,21 +1,27 @@
 ﻿window.lessonData = {
-  "title": "Mini Test: Part 1 (3 Câu)",
+  "title": "Mini Test: Part 1",
   "part": 1, 
   "items": [
     // --- CÂU HỎI SỐ 1 ---
     {
       "image": "q1.jpg",
       "audio": "audio_q1.mp3",
-      "transcript": "(A) They are shaking hands.\n(B) They are walking together.",
+      "transcript": `(A) She is sweeping the ground. \n
+                    (B) She is watering the plants. \n 
+                    (C) She is opening the gate. \n
+                    (D) She is carrying a box.`,
       "options": ["(A)", "(B)", "(C)", "(D)"],
-      "answer": 1
+      "answer": 0
     }, // <--- Nhớ có dấu phẩy ở đây để ngăn cách
 
     // --- CÂU HỎI SỐ 2 ---
     {
       "image": "q2.jpg",
       "audio": "audio_q2.mp3",
-      "transcript": "The car is parked near the tree.",
+      "transcript": `(A) He is reading a menu.  \n
+(B) He is paying the bill. \n
+(C) He is serving food. \n
+(D) He is cleaning the table.`,
       "options": ["(A)", "(B)", "(C)", "(D)"]
     }, // <--- Dấu phẩy tiếp
 
@@ -23,12 +29,16 @@
     {
       "image": "q3.jpg",
       "audio": "audio_q3.mp3",
-      "transcript": "A woman is looking at the menu.",
+      "transcript": `(A) They are climbing a ladder. \n
+(B) They are painting the wall. \n
+(C) They are turning off the lights. \n
+(D) They are sitting on the floor.`,
       "options": ["(A)", "(B)", "(C)", "(D)"]
     }
     // Câu cuối cùng thì KHÔNG cần dấu phẩy
   ]
 };
+
 
 
 
