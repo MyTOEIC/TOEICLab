887fbf1ca1c81ab35a3bd4ae3208ca39f3095a37
MyTOEIC <ptra28082002@gmail.com>
2026-02-12 19:54:21 +0700

    Update data.js

--- data/nghe/bai-1/data.js
+++ data/nghe/bai-1/data.js
@@ -1,11 +1,12 @@
 ﻿window.lessonData ={
-  "transcript": `(A) She is sweeping the ground.
-(B) She is watering the plants.
-(C) She is opening the gate.
+  "transcript": `(A) She is sweeping the ground. \n
+(B) She is watering the plants. \n
+(C) She is opening the gate. \n
 (D) She is carrying a box.`,
   "image": "hinh-anh.jpg",
   "audio": "am-thanh.mp3"
 };
+
 
 
 
