7fef7927e4c34472a417f98088f4da18e59a9435
MyTOEIC <ptra28082002@gmail.com>
2026-02-12 19:53:01 +0700

    Update data.js

--- data/nghe/bai-1/data.js
+++ data/nghe/bai-1/data.js
@@ -3,8 +3,9 @@
 (B) She is watering the plants.
 (C) She is opening the gate.
 (D) She is carrying a box.`,
-  "image": "hinh-anh.png",
+  "image": "hinh-anh.jpg",
   "audio": "am-thanh.mp3"
 };
+
 
 
