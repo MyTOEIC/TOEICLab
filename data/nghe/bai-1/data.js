3ae1d2750a31a05b3bc3b95d298fa6ba770ffed6
MyTOEIC <ptra28082002@gmail.com>
2026-02-12 21:47:21 +0700

    Update data.js

--- data/nghe/bai-1/data.js
+++ data/nghe/bai-1/data.js
@@ -36,10 +36,92 @@
 (D) They are sitting on the floor.`,
       "options": ["(A)", "(B)", "(C)", "(D)"], 
       "answer": 0
-    }
+    },
+// --- CÂU HỎI SỐ 4 ---
+    {
+      "image": "q4.jpg",
+      "audio": "audio_q4.mp3",
+      "transcript": `(A) He is reading a menu.  \n
+(B) He is paying the bill. \n
+(C) He is serving food. \n
+(D) He is cleaning the table.`,
+      "options": ["(A)", "(B)", "(C)", "(D)"], 
+      "answer": 0
+    },
+// --- CÂU HỎI SỐ 5 ---
+    {
+      "image": "q5.jpg",
+      "audio": "audio_q5.mp3",
+      "transcript": `(A) She is checking out customers.
+(B) She is arranging items on a shelf.
+(C) She is closing the store.
+(D) She is carrying a shopping basket.`,
+      "options": ["(A)", "(B)", "(C)", "(D)"], 
+      "answer": 0
+    },
+    // --- CÂU HỎI SỐ 6 ---
+    {
+      "image": "q6.jpg",
+      "audio": "audio_q6.mp3",
+      "transcript": `(A) He is reading a menu.  \n
+(B) He is paying the bill. \n
+(C) He is serving food. \n
+(D) He is cleaning the table.`,
+      "options": ["(A)", "(B)", "(C)", "(D)"], 
+      "answer": 0
+    },
+
+    // --- CÂU HỎI SỐ 7 ---
+    {
+      "image": "q7.jpg",
+      "audio": "audio_q7.mp3",
+      "transcript": `(A) He is reading a menu.  \n
+(B) He is paying the bill. \n
+(C) He is serving food. \n
+(D) He is cleaning the table.`,
+      "options": ["(A)", "(B)", "(C)", "(D)"], 
+      "answer": 0
+    },
+
+    // --- CÂU HỎI SỐ 8 ---
+    {
+      "image": "q8.jpg",
+      "audio": "audio_q8.mp3",
+      "transcript": `(A) He is reading a menu.  \n
+(B) He is paying the bill. \n
+(C) He is serving food. \n
+(D) He is cleaning the table.`,
+      "options": ["(A)", "(B)", "(C)", "(D)"], 
+      "answer": 0
+    },
+
+    // --- CÂU HỎI SỐ 9 ---
+    {
+      "image": "q9.jpg",
+      "audio": "audio_q9.mp3",
+      "transcript": `(A) He is reading a menu.  \n
+(B) He is paying the bill. \n
+(C) He is serving food. \n
+(D) He is cleaning the table.`,
+      "options": ["(A)", "(B)", "(C)", "(D)"], 
+      "answer": 0
+    },
+
+    // --- CÂU HỎI SỐ 10 ---
+    {
+      "image": "q10.jpg",
+      "audio": "audio_q10.mp3",
+      "transcript": `(A) He is reading a menu.  \n
+(B) He is paying the bill. \n
+(C) He is serving food. \n
+(D) He is cleaning the table.`,
+      "options": ["(A)", "(B)", "(C)", "(D)"], 
+      "answer": 0
+    },
     // Câu cuối cùng thì KHÔNG cần dấu phẩy
   ]
 };
+
 
 
 
