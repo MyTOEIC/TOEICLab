2471c9d2c869508f6e47ae0981b95be968b2c483
MyTOEIC <ptra28082002@gmail.com>
2026-02-22 23:58:23 +0700

    Update data.js

--- data/tu-vung/bai-2/data.js
+++ data/tu-vung/bai-2/data.js
@@ -5,7 +5,15 @@
   Consumers tend to consume many products that follow a fad, but to convince them to stay loyal requires skillful persuasion. 
   Currently, many brands seek inspiration from real needs to develop more productive products. When customers feel satisfaction, they become more loyal to the brand. 
   As a result, businesses not only chase temporary trends but also build sustainable value.`,
-  "items": [
+
+   "content_vi": `Trong thị trường hiện nay, các công ty luôn tìm cách hấp dẫn khách hàng bằng những chiến lược mới. 
+   Họ thường so sánh sản phẩm của mình với đối thủ để tạo lợi thế trong cạnh tranh. 
+   Người tiêu dùng thường tiêu thụ nhiều sản phẩm theo mốt nhất thời, nhưng để thuyết phục họ gắn bó lâu dài thì cần có lời thuyết phục khéo léo. 
+   Hiện tại, nhiều thương hiệu tìm nguồn cảm hứng từ nhu cầu thực tế để phát triển sản phẩm có hiệu quả hơn. 
+   Khi khách hàng cảm thấy sự thỏa mãn, họ sẽ trung thành hơn với thương hiệu. 
+   Nhờ vậy, doanh nghiệp không chỉ chạy theo xu hướng nhất thời mà còn xây dựng giá trị bền vững.`,
+   
+   "items": [
     { "en": "Office", "vi": "Văn phòng", "pron": "/'ɔfis/" },
     { "en": "Manager", "vi": "Quản lý", "pron": "/'mænidʒə/" },
     { "en": "Colleague", "vi": "Đồng nghiệp", "pron": "/'kɔli:g/" },
@@ -13,3 +21,4 @@
   ]
 
 };
+
