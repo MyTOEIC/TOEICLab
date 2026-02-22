9cdc2b874b03f59353c12245776e0719455c779b
MyTOEIC <ptra28082002@gmail.com>
2026-02-22 23:57:05 +0700

    Update data.js

--- data/tu-vung/bai-2/data.js
+++ data/tu-vung/bai-2/data.js
@@ -1,10 +1,15 @@
 ﻿window.lessonData = {
   "title": "Chủ đề: Office (Văn phòng)",
-  "content": "In a modern <b>office</b>, employees often use <b>copiers</b> and <b>fax machines</b>. The <b>manager</b> is responsible for supervising the team.",
+  "content": `In the current market, companies always try to attract customers with new strategies. 
+  They often compare their products with competitors to gain an advantage in the competition. 
+  Consumers tend to consume many products that follow a fad, but to convince them to stay loyal requires skillful persuasion. 
+  Currently, many brands seek inspiration from real needs to develop more productive products. When customers feel satisfaction, they become more loyal to the brand. 
+  As a result, businesses not only chase temporary trends but also build sustainable value.`,
   "items": [
     { "en": "Office", "vi": "Văn phòng", "pron": "/'ɔfis/" },
     { "en": "Manager", "vi": "Quản lý", "pron": "/'mænidʒə/" },
     { "en": "Colleague", "vi": "Đồng nghiệp", "pron": "/'kɔli:g/" },
     { "en": "Equipment", "vi": "Thiết bị", "pron": "/i'kwipmənt/" }
   ]
+
 };
