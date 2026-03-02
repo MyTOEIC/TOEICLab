8cc7ed6792087cb16d9fdf68f60bce1023111411
MyTOEIC <ptra28082002@gmail.com>
2026-03-02 14:50:52 +0700

    Update data.js

--- data/tu-vung/bai-2/data.js
+++ data/tu-vung/bai-2/data.js
@@ -14,12 +14,21 @@
    Nhờ vậy, doanh nghiệp không chỉ chạy theo xu hướng nhất thời mà còn xây dựng giá trị bền vững.`,
    
    "items": [
-    { "en": "Office", "vi": "Văn phòng", "pron": "/'ɔfis/" },
-    { "en": "Manager", "vi": "Quản lý", "pron": "/'mænidʒə/" },
-    { "en": "Colleague", "vi": "Đồng nghiệp", "pron": "/'kɔli:g/" },
-    { "en": "Equipment", "vi": "Thiết bị", "pron": "/i'kwipmənt/" }
-  ]
+  { "en": "Attract", "type": "(v)", "vi": "hấp dẫn, lôi cuốn", "pron": "/əˈtrækt/" },
+  { "en": "Compare", "type": "(v)", "vi": "so sánh", "pron": "/kəmˈper/" },
+  { "en": "Competition", "type": "(n)", "vi": "cuộc thi, sự cạnh tranh", "pron": "/ˌkɑːmpəˈtɪʃən/" },
+  { "en": "Consume", "type": "(v)", "vi": "tiêu thụ", "pron": "/kənˈsuːm/" },
+  { "en": "Convince", "type": "(v)", "vi": "thuyết phục", "pron": "/kənˈvɪns/" },
+  { "en": "Currently", "type": "(adv)", "vi": "hiện tại", "pron": "/ˈkɜːrəntli/" },
+  { "en": "Fad", "type": "(n)", "vi": "ham mê, mốt nhất thời", "pron": "/fæd/" },
+  { "en": "Inspiration", "type": "(n)", "vi": "nguồn cảm hứng, sự hít vào", "pron": "/ˌɪnspəˈreɪʃən/" },
+  { "en": "Market", "type": "(n)", "vi": "chợ, thị trường", "pron": "/ˈmɑːrkɪt/" },
+  { "en": "Persuasion", "type": "(n)", "vi": "lời thuyết phục", "pron": "/pərˈsweɪʒən/" },
+  { "en": "Productive", "type": "(adj)", "vi": "có hiệu quả", "pron": "/prəˈdʌktɪv/" },
+  { "en": "Satisfaction", "type": "(n)", "vi": "sự làm thỏa mãn", "pron": "/ˌsætɪsˈfækʃən/" }
+]
 
 };
+
 
 
