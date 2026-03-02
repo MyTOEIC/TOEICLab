b726b3ff11364a7f7edcc19f7c4e9e11f0f814ef
MyTOEIC <ptra28082002@gmail.com>
2026-03-02 15:45:47 +0700

    Create data.js

---
+++ data/tu-vung/bai-4/data.js
@@ -0,0 +1,15 @@
+"items": [
+  { "en": "Address", "type": "(n)", "vi": "địa chỉ", "pron": "/ˈædres/" },
+  { "en": "Avoid", "type": "(v)", "vi": "tránh", "pron": "/əˈvɔɪd/" },
+  { "en": "Demonstrate", "type": "(v)", "vi": "trình bày, chứng minh", "pron": "/ˈdemənstreɪt/" },
+  { "en": "Develop", "type": "(v)", "vi": "phát triển", "pron": "/dɪˈveləp/" },
+  { "en": "Evaluate", "type": "(v)", "vi": "đánh giá, định giá", "pron": "/ɪˈvæljueɪt/" },
+  { "en": "Gather", "type": "(v)", "vi": "tụ họp", "pron": "/ˈɡæðər/" },
+  { "en": "Offer", "type": "(n)", "vi": "lời đề nghị", "pron": "/ˈɔːfər/" },
+  { "en": "Primarily", "type": "(adv)", "vi": "chủ yếu, trước hết", "pron": "/praɪˈmerəli/" },
+  { "en": "Risk", "type": "(n)", "vi": "nguy cơ", "pron": "/rɪsk/" },
+  { "en": "Strategy", "type": "(n)", "vi": "chiến lược", "pron": "/ˈstrætədʒi/" },
+  { "en": "Strong", "type": "(adj)", "vi": "cường tráng", "pron": "/strɔːŋ/" },
+  { "en": "Substitution", "type": "(n)", "vi": "sự thay thế", "pron": "/ˌsʌbstɪˈtuːʃən/" }
+]
+}
