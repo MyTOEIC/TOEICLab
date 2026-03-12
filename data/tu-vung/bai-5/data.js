bd2cac4222f04d5f2f094279d39255ec8deb3f8c
MyTOEIC <ptra28082002@gmail.com>
2026-03-12 21:55:28 +0700

    Create data.js

---
+++ data/tu-vung/bai-5/data.js
@@ -0,0 +1,14 @@
+"items": [
+  { "en": "Accommodate", "type": "(v)", "vi": "làm phù hợp, điều tiết", "pron": "/əˈkɑːmədeɪt/" },
+  { "en": "Arrangement", "type": "(n)", "vi": "sự thu xếp, sự bố trí", "pron": "/əˈreɪndʒmənt/" },
+  { "en": "Association", "type": "(n)", "vi": "đoàn thể, sự kết hợp", "pron": "/əˌsoʊsiˈeɪʃən/" },
+  { "en": "Attend", "type": "(v)", "vi": "tham dự, có mặt", "pron": "/əˈtend/" },
+  { "en": "Get in touch", "type": "(ph.v)", "vi": "liên lạc với", "pron": "/ɡet ɪn tʌtʃ/" },
+  { "en": "Hold", "type": "(v)", "vi": "cầm giữ", "pron": "/hoʊld/" },
+  { "en": "Location", "type": "(n)", "vi": "vị trí, địa điểm", "pron": "/loʊˈkeɪʃən/" },
+  { "en": "Overcrowded", "type": "(adj)", "vi": "chật", "pron": "/ˌoʊvərˈkraʊdɪd/" },
+  { "en": "Register", "type": "(n)", "vi": "sự đăng ký", "pron": "/ˈredʒɪstər/" },
+  { "en": "Select", "type": "(v)", "vi": "lựa chọn", "pron": "/sɪˈlekt/" },
+  { "en": "Session", "type": "(n)", "vi": "buổi họp", "pron": "/ˈseʃən/" },
+  { "en": "Take part in", "type": "(ph.v)", "vi": "tham gia", "pron": "/teɪk pɑːrt ɪn/" }
+]
