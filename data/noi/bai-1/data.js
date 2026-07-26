939333a466423165c0eb826f5e0adbf25e5f82fd
MyTOEIC <ptra28082002@gmail.com>
2026-07-26 22:27:39 +0700

    Update data.js

--- data/noi/bai-1/data.js
+++ data/noi/bai-1/data.js
@@ -1,30 +1,13 @@
 ﻿window.lessonData = {
-  "title": "Speaking Part 2: Describe a picture",
-  
-  // Tên file ảnh bạn đã chép vào folder
-  "image": "hinh-anh.jpg", 
-  
-  // Tên file audio mẫu (Sample answer)
+  "title": "Speaking Part 1: Read a Text Aloud",
+  "image": "", 
   "audio": "amthanh.mp3",
-  
-  // (Tùy chọn) Transcript bài nói mẫu để bạn tham khảo
+  // Transcript bài nói mẫu để bạn tham khảo
   "content": `This Sunday, Jazz Master Coffee Shop invites coffee lovers to a tasting event at our store. 
     Visit the shop between 6 and 9 o'clock to enjoy half price desserts, live music, and our exclusive coffee from Columbia.
    While you're here, you can complete a customer survey to receive a pack of free coffee to brew at home
     `
-
 },
- {
-   "image": "hinh-anh.jpg", 
-  
-  // Tên file audio mẫu (Sample answer)
-  "audio": "amthanh.mp3",
-  
-  // (Tùy chọn) Transcript bài nói mẫu để bạn tham khảo
-  "content": `This Sunday, Jazz Master Coffee Shop invites coffee lovers to a tasting event at our store. 
-    Visit the shop between 6 and 9 o'clock to enjoy half price desserts, live music, and our exclusive coffee from Columbia.
-   While you're here, you can complete a customer survey to receive a pack of free coffee to brew at home `
- }
    ;
 
 
